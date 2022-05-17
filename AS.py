3f324d43ee6c98b153f60d4a7a0e3ef0ec74bae5
devashish0007 <devashish.dewangan@ssipmt.com>
2022-05-17 15:55:43 +0530

    Update AS.py

--- AS.py
+++ AS.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 import argparse
 import datetime
+import os
+import socket
+import sys
+from multiprocessing import Process
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.concatkdf import ConcatKDFHash
 from cryptography.hazmat.primitives.ciphers.aead import AESCCM
@@ -8,86 +12,115 @@ import asyncio
 import websockets
 import threading
 
+seperator = b'+'
+
+
+def key_generator(key):
+    # Using PRF to generate IV
+    aesccm = AESCCM(key)
+    ckdf = ConcatKDFHash(algorithm=hashes.SHA256(), length=13, otherinfo=None)
+    iv = ckdf.derive(key)
+    return aesccm, iv
+
 
 def add_user():
     username = input('Enter username : ')
     key = AESCCM.generate_key(bit_length=128)
-    print('Username : ', username, 'Password is : ',key)
-    with open(f"pask_{username}.txt", 'wb') as fu:
+    print('Username : ', username, '\nPassword is : ', key)
+    with open(f"psk_{username}.txt", 'wb') as fu:
         fu.write(key)
     # add username & password to block chain
 
 
 def modify():
+    try:
+        while True:
+            task = input()
+            if task.lower() == 'add':
+                add_user()
+            elif task.lower() == 'exit':
+                print("To stop AS enter ctrl + c to exit...")
+                break
+    except:
+        print("AS Stopped....")
 
-    while True:
-        task = input('1. Enter add to add user\n2. Enter exit to stop...\n')
-        if task.lower() == 'add':
-            add_user()
-        elif task.lower() == 'exit':
-            print("To stop AS enter ctrl + c...\n")
-            break
+def response(clientSocket, clientAddr):
 
-
-async def response(websocket, path):
-    # psk_AP key retrieve wrt id...
+# try:
     psk_AP = ''
     with open('psk_AP.txt', 'rb') as f:
         psk_AP = f.read()
 
-    message = await websocket.recv()
-    ct = message[8:]
-    # 8 byte of id
-    id = message[0:8]
-    # key retrieve wrt id...
-    print(f"Retrieving key from blockchain for {id}...")
-    f1 = open('psk_c.txt', 'rb')
-    psk_c = f1.read()
-    # key retrieve end
-    # Using PRF to generate IV
+    message = clientSocket.recv(1024)
 
-    aesccm_c = AESCCM(psk_c)
-    ckdf_c = ConcatKDFHash(algorithm=hashes.SHA256(), length=13, otherinfo=None)
-    iv_c = ckdf_c.derive(psk_c)
+    ct = message.split(seperator)
+    # print(ct)
+    id = ct[1]
+    ct = ct[2]
+    # ct = ct[3]
 
-    message = aesccm_c.decrypt(iv_c, ct, None)
-    id_v = message[0:8]
-    nonce = message[8:14]
-    mode = message[14:15]
-    validity = int(message[15:].decode())
-    if (id == id_v):
-        # user verified
-        sk = AESCCM.generate_key(bit_length=128)
-        if mode.lower() == 'h':
-            date = datetime.datetime.now()
-            date = date + datetime.timedelta(hours=validity)
-            expiryTime = date.strftime("%Y-%m-%d %H:%M:%S")
+    # Retrive client key from blockchain
+    with open(f'psk_{id.decode()}.txt', 'rb') as fc:
+        psk_c = fc.read()
 
-        elif mode.lower() == 'd':
-            date = datetime.datetime.now()
-            date = date + datetime.timedelta(days=validity)
-            expiryTime = date.strftime("%Y-%m-%d %H:%M:%S")
+    # print(ct)
+    aesccm, iv = key_generator(psk_c)
+    try:
 
-        token = sk + id + expiryTime
-        # Using PRF to generate IV
-        aesccm_AP = AESCCM(psk_AP)
-        ckdf_AP = ConcatKDFHash(algorithm=hashes.SHA256(), length=13, otherinfo=None)
-        iv_AP = ckdf_AP.derive(psk_AP)
+        request = aesccm.decrypt(iv, ct, None)
+        request = request.split(seperator)
 
-        token = aesccm_AP.encrypt(iv_AP, token, None)
+        nonce = request[1]
+        if id == request[0]:
+            # Using PRF to generate IV
 
-        message = sk + id + nonce + token
-        ct = aesccm_c.encrypt(iv_c, message, None)
-        ct = b'800' + ct + token
-        await websocket.send(ct)
-    else:
-        await websocket.send(b'801' + (nonce + b'Invalid Credentials'))
+            mode = request[2]
+            validity = int(request[3].decode())
+
+            # user verified
+            sk = AESCCM.generate_key(bit_length=128)
+            if mode.lower() == b'h':
+                date = datetime.datetime.now()
+                date = date + datetime.timedelta(hours=validity)
+                expiryTime = date.strftime("%Y-%m-%d %H:%M:%S")
+
+            elif mode.lower() == b'd':
+                date = datetime.datetime.now()
+                date = date + datetime.timedelta(days=validity)
+                expiryTime = date.strftime("%Y-%m-%d %H:%M:%S")
+
+            token = sk + seperator + id + seperator + expiryTime.encode()
+            # Using PRF to generate IV
+            aesccm_AP = AESCCM(psk_AP)
+            ckdf_AP = ConcatKDFHash(algorithm=hashes.SHA256(), length=13, otherinfo=None)
+            iv_AP = ckdf_AP.derive(psk_AP)
+            # print(token)
+            token = aesccm_AP.encrypt(iv_AP, token, None)
+            # print(token)
+            # message = sk + seperator + id + seperator + nonce + seperator + token
+            message = sk + seperator + id + seperator + nonce
+            ct = aesccm.encrypt(iv, message, None)
+            # print(ct)
+            ct = b'800' + seperator + id + seperator + ct + seperator + token
+            print(ct)
+            clientSocket.send(ct)
+            # await websocket.send(ct)
+            print(f"user: {id.decode()} is Authenticated...")
+        else:
+            clientSocket.send(b'801' + seperator + id + seperator + nonce + seperator + b'Invalid Credentials')
+            print(f"user: {id.decode()}  Authentication failed...")
+            # await websocket.send(b'801' + (nonce + b'Invalid Credentials'))
+    except:
+        nonce = os.urandom(6)
+        clientSocket.send(b'801' + seperator + id + seperator + nonce + seperator +b'Invalid Credentials')
+        print(f"user: {id.decode()}  Authentication failed...")
+    # print("Successful")
+    clientSocket.close()
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-
     parser.add_argument('-p', type=int)
 
     args = parser.parse_args()
@@ -96,18 +129,34 @@ if __name__ == '__main__':
         with open('psk_AP.txt', 'rb') as f:
             psk_AP = f.read()
     except:
-        psk_AP = AESCCM.generate_key(bit_length=256)
+        psk_AP = AESCCM.generate_key(bit_length=128)
         with open('psk_AP.txt', 'wb') as f:
             f.write(psk_AP)
+
+    print("AS Running...")
+
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    serverSocket.bind(('', args.p))
+
+    # socket listening
+    serverSocket.listen(20)
+
+    t = threading.Thread(target=modify, args=())
+    t.start()
     try:
-        start_server = websockets.serve(response, 'localhost', args.p)
-        print("AS Running...")
-
-        t = threading.Thread(target=modify, args=())
-        t.start()
-
-        asyncio.get_event_loop().run_until_complete(start_server)
-        asyncio.get_event_loop().run_forever()
+        while True:
+            print("Type Ctrl - C to Stop AS...\n1. Enter add to add user\n2. Enter exit to stop...")
+            # print(serverSocket)
+            clientSocket, clientAddr = serverSocket.accept()
+            # print(clientSocket)
+            print("connection established...")
+            # t = establishConnection(clientSocket, clientAddr)
+            response(clientSocket, clientAddr)
+            # p = Process(target=response, args=(clientSocket, clientAddr))
+            # p.start()
+            # print("Thread Created...")
 
     except:
-        print("\nAS stopped...")
+        serverSocket.close()
+        print("AS stopped...")
